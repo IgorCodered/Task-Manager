c49a2b3d84497835dc0200a6a330e3e1ad1f0e44
Igor Elesin <collidefor@outlook.com>
2023-11-06 00:06:21 +0700

    Архитектура приложения 0.1

--- src/SubTask.java
+++ src/SubTask.java
@@ -3,6 +3,7 @@ public class SubTask extends Task implements TaskInterface
     public SubTask() {
         SubTask now = new SubTask();
         now.setTaskStatus(TaskStatus.NEW.ordinal());
+        System.out.println("Строка тест");
     }
 
     public SubTask(String name, int taskStatus) {
